45e0fd4a62374c436e9248afb2bea3d831bf05a3
soworksl <jimy.waner11@gmail.com>
2024-01-22 15:05:25 -0400

    update project model

--- src/lib/models/project.js
+++ src/lib/models/project.js
@@ -1,8 +1,10 @@
 export default class Project{
-    constructor({ id, showName, imgUri, techs }){
+    constructor({ id, showName, imgUri, description, tagInfo, techs }){
         this.id = id;
         this.showName = showName;
         this.imgUri = imgUri;
+        this.description = description;
+        this.tagInfo = tagInfo;
         this.techs = techs;
     }
 }
